aa38fa4a9d09e43b9bde7c2058f379be49bf6162
Gabriel Henrique <gabrielcomputacao@outlook.com>
2025-11-30 06:51:35 -0300

    feat: creating server using http library

---
+++ apiNode/server/routes.js
@@ -0,0 +1,9 @@
+
+
+ const routes = {
+    "/": "Curso de Node Js dashboard",
+    "/livros": "Rota Livros",
+    "/biblioteca": "Rota biblioteca",
+}
+
+export default routes;
--- apiNode/server/server.js
+++ apiNode/server/server.js
@@ -1,14 +1,15 @@
 
 import http from 'http'
+import routes from './routes.js'
 
 const PORT = 3005
 
 const server =http.createServer((req,res) => {
     res.writeHead(200, { "content-type" : 'text/plain' })
-    res.end("curso node")
+    res.end(routes[req.url])
 } )
 
 server.listen( PORT, () =>{
-    console.log('servidor escutando');
+    console.log(`servidor escutando na porta ${PORT}`);
     
 } )
