a83c4f4813ed5263571e9782deff06d907fa6aeb
Gabriel Henrique <gabrielcomputacao@outlook.com>
2025-11-30 07:51:29 -0300

    feat> create crud in path livros

--- apiNode/server/routes.js
+++
@@ -1,9 +0,0 @@
-
-
- const routes = {
-    "/": "Curso de Node Js dashboard",
-    "/livros": "Rota Livros",
-    "/biblioteca": "Rota biblioteca",
-}
-
-export default routes;
--- apiNode/server/server.js
+++ apiNode/server/server.js
@@ -1,15 +1,11 @@
 
-import http from 'http'
-import routes from './routes.js'
+
+import app from '../src/app.js'
 
 const PORT = 3005
 
-const server =http.createServer((req,res) => {
-    res.writeHead(200, { "content-type" : 'text/plain' })
-    res.end(routes[req.url])
-} )
 
-server.listen( PORT, () =>{
+app.listen( PORT, () =>{
     console.log(`servidor escutando na porta ${PORT}`);
     
 } )
