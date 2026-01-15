a36e8e959916e2b3bc101fa06d79e9d64c6e65a4
Gabriel Henrique <gabrielcomputacao@outlook.com>
2026-01-15 06:36:03 -0300

    feat: create custom validators in the mongoose

--- apiNode/src/models/Author.js
+++ apiNode/src/models/Author.js
@@ -5,7 +5,17 @@ const authorSchema = mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId},
     // moongose já tem essa funcionalidade de colocar a mensagem de erro personalizada nesses campos
     name: { type: String, required: [true, "O nome do autor deve ser obrigatório"]},
-    nationality: { type: String}
+    nationality: { type: String},
+    // * Validação personalizada
+    age: { 
+        type: Number,
+        validate: {
+            validator: (value) =>{
+            return value > 5 && value < 150
+        },
+        message: "Não pode ser maior que 150. Valor Fornecido {VALUE}"
+    }
+     }
 }, {versionKey: false});
 
 const author = mongoose.model("authors", authorSchema);
